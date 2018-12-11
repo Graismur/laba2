bc1e7ebb9d2708d2ad980136a11172978a26629d
Graismur <yooopolina@mail.ru>
2018-12-11 21:46:52 +0300

    laba2

---
+++ sss.h
@@ -0,0 +1,10 @@
+﻿#pragma once 
+#include <stdio.h> 
+#include <tchar.h> 
+#include <iostream> 
+#include <cstdlib> 
+#include <string> 
+#include <malloc.h> 
+#include <fstream> 
+using namespace std;
+void sss(string *name, int e); 
